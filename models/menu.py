0d7986150337c1f225bc3108b5bd4a8523afdca5
asomnassar <133059610+asomnassar@users.noreply.github.com>
2023-05-19 15:04:52 +0300

    Add files via upload

---
+++ models/menu.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+# ----------------------------------------------------------------------------------------------------------------------
+# this is the main application menu add/remove items as required
+# ----------------------------------------------------------------------------------------------------------------------
+
+response.menu = [
+    (T('Home'), False, URL('default', 'index'), []),
+    (T('Schedules'), False, URL('regcont', 'schedules'), []),
+    (T('Add Scehdule'), False, URL('regcont', 'addSchedule'), []),
+    (T('Courses'), False, URL('regcont', 'courses'), []),
+    (T('Add Course'), False, URL('regcont', 'addCourse'), []),
+
+
+]
+
+# ----------------------------------------------------------------------------------------------------------------------
+# provide shortcuts for development. you can remove everything below in production
+# ----------------------------------------------------------------------------------------------------------------------
+
+if not configuration.get('app.production'):
+    _app = request.application
+    response.menu += [
+        (T('My Sites'), False, URL('admin', 'default', 'site')),
+        
+    ]
